6c7ae2c0034fcf16247c3f0f110b26b41cd0cc4c
Ahmad Ilham M <1206009@sttgarut.ac.id>
2015-05-18 17:33:30 +0700

    Update Aplikasi_pulsa.java

--- src/aplikasi_pulsa/Aplikasi_pulsa.java
+++ src/aplikasi_pulsa/Aplikasi_pulsa.java
@@ -6,11 +6,16 @@ public class Aplikasi_pulsa {
     }
 
     public void login() {
-        String username, pasworrd;
+        String username, passworrd;
     }
 
     public void master() {
         int no, kode, harga;
         String operator;
     }
+    public void agen() {
+        int id;
+        String nama, alamat;
+    }
+    
 }
