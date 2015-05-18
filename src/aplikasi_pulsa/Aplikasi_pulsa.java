e72244543d9e88481a753f57fd46a7a1b2a9d430
anas_kariman <1206012@sttgarut.ac.id>
2015-05-18 13:30:27 +0700

    menambah class master

--- src/aplikasi_pulsa/Aplikasi_pulsa.java
+++ src/aplikasi_pulsa/Aplikasi_pulsa.java
@@ -1,12 +1,16 @@
 package aplikasi_pulsa;
+
 public class Aplikasi_pulsa {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
- public void login() {
- String username,pasworrd;}   
+
+    public void login() {
+        String username, pasworrd;
+    }
+
+    public void master() {
+        int no, kode, harga;
+        String operator;
+    }
 }
